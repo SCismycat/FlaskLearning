351e5d3015eb145b2c58699e7d094f4cadda35f9
leslee <lifei@gaodun.com>
2019-11-29 17:58:49 +0800

    添加注释

--- Flask_Tutorial/flasky/flasky.py
+++ Flask_Tutorial/flasky/flasky.py
@@ -23,13 +23,13 @@ app.config.from_envvar('FLASKY_SETTINGS',silent=True)
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
-
+# 创建一个初始化数据库的函数。
 def init_db():
     with closing(connect_db()) as db:
         with app.open_resource('schema.sql',mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
-
+# 该装饰器装饰的函数，会在请求之前调用，且不传递参数。
 @app.before_request
 def before_request():
     g.db = connect_db()
@@ -77,20 +77,6 @@ def logout():
     session.pop('logged_in',None)
     flash('You were logged out')
     return redirect(url_for('show_entires'))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
